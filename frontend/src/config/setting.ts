21f5534501b21862ded1a3e8e82748549a34c7bc
lackone <lackone@126.com>
2026-03-23 15:37:46 +0800

    添加

--- frontend/src/config/setting.ts
+++ frontend/src/config/setting.ts
@@ -56,7 +56,7 @@ export const SETTING_DEFAULT_CONFIG = {
   /** 是否显示语言切换 */
   showLanguage: true,
   /** 是否显示进度条 */
-  showNprogress: false,
+  showNprogress: true,
   /** 是否显示设置引导 */
   showSettingGuide: true,
   /** 是否显示节日文本 */
